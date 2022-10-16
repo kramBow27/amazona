23b284661cb3d06a7c7015290c1619cbc6a51b3b
kramBow27 <kram.bow.instanceof@gmail.com>
2022-10-16 16:31:52 -0300

    Product-component-Done

--- frontend/src/Components/product.js
+++ frontend/src/Components/product.js
@@ -1,11 +1,12 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 function Product(props) {
   const { product } = props;
   return (
-    <Card key={product.slug}>
+    <Card>
       <Link to={`/product/${product.slug}`}>
         <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
@@ -13,6 +14,7 @@ function Product(props) {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
         <Button>Add to cart</Button>
       </Card.Body>
